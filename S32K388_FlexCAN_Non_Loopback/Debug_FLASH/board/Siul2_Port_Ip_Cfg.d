4f911e482205daaae2eb0545ba18dd63350be6fe
KJ-Sagar <kjagannathsagar@gmail.com>
2026-08-27 15:34:09 +0530

    Added NON_Loopback folder for backup

---
+++ S32K388_FlexCAN_Non_Loopback/Debug_FLASH/board/Siul2_Port_Ip_Cfg.d
@@ -0,0 +1,23 @@
+board/Siul2_Port_Ip_Cfg.o: ../board/Siul2_Port_Ip_Cfg.c \
+ ../board/Siul2_Port_Ip_Cfg.h \
+ /home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/header/S32K388_SIUL2.h \
+ /home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/header/S32K388_COMMON.h \
+ ../RTD/include/Siul2_Port_Ip_Types.h \
+ /home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Std_Types.h \
+ /home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Platform_Types.h \
+ /home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Compiler.h \
+ /home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Compiler_Cfg.h \
+ /home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/CompilerDefinition.h \
+ /home/kj/workspaceS32DS.3.6.7/workspaceS32DS.3.6.7/FlexCAN_Ip_Example_S32K388/generate/include/Siul2_Port_Ip_Defines.h \
+ /home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/header/S32K388_DCM_GPR.h
+../board/Siul2_Port_Ip_Cfg.h:
+/home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/header/S32K388_SIUL2.h:
+/home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/header/S32K388_COMMON.h:
+../RTD/include/Siul2_Port_Ip_Types.h:
+/home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Std_Types.h:
+/home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Platform_Types.h:
+/home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Compiler.h:
+/home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Compiler_Cfg.h:
+/home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/CompilerDefinition.h:
+/home/kj/workspaceS32DS.3.6.7/workspaceS32DS.3.6.7/FlexCAN_Ip_Example_S32K388/generate/include/Siul2_Port_Ip_Defines.h:
+/home/kj/NXP/S32DS.3.6.7/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/header/S32K388_DCM_GPR.h:
